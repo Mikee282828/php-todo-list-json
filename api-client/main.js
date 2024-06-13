1ecbacb3dfacffd17f68e901d53f2160fb63895d
michele laptop <micheledong2803@gmail.com>
2024-06-13 18:53:09 +0200

    styled

--- api-client/main.js
+++ api-client/main.js
@@ -3,14 +3,45 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            tasks: "",
+            tempTitle: "",
         }
     },
-    methods:{
-
+    methods: {
+        checkDone(bool) {
+            if (bool) {
+                return "done";
+            } else {
+                return "undone";
+            }
+        },
+        switchBool(bool, index) {
+            if (bool) {
+                this.tasks[index].done = false;
+            } else {
+                this.tasks[index].done = true;
+            }
+        },
+        checkBkg(bool) {
+            if (bool) {
+                return "checkActive"
+            } else {
+                return ""
+            }
+        },
+        addTask(titolo) {
+            let task = {
+                "title" : titolo,
+                "done" : false,
+                "Date" : Date.now,
+            };
+            this.tasks.unshift(task);
+            this.tempTitle = "";
+        }
     },
-    mounted(){
-        axios.get("../server.php").then(result=>{
-            console.log(result.data );
+    mounted() {
+        axios.get("../server.php").then(result => {
+            this.tasks = result.data;
         })
     }
 }).mount('#app')
--- server.php
+++ server.php
@@ -2,20 +2,80 @@
 
 $prova = [
     [
-        "task" => "prendere le medicine",
-        "done" => false,
+        "title" => "prendere le medicine",
+        "done" => true,
         "Date" => "13/06/2024 18:00"
     ],
     [
-        "task" => "prendere le medicine 2",
+        "title" => "prendere le medicine 1",
         "done" => false,
         "Date" => "13/06/2024 19:00"
     ],
     [
-        "task" => "prendere le medicine 3",
+        "title" => "prendere le medicine 2",
+        "done" => true,
+        "Date" => "13/06/2024 20:00"
+    ],
+    [
+        "title" => "prendere le medicine 3",
+        "done" => true,
+        "Date" => "13/06/2024 20:00"
+    ],
+    [
+        "title" => "prendere le medicine 4",
         "done" => false,
         "Date" => "13/06/2024 20:00"
-    ]
+    ],
+    [
+        "title" => "prendere le medicine",
+        "done" => true,
+        "Date" => "13/06/2024 18:00"
+    ],
+    [
+        "title" => "prendere le medicine 1",
+        "done" => false,
+        "Date" => "13/06/2024 19:00"
+    ],
+    [
+        "title" => "prendere le medicine 2",
+        "done" => true,
+        "Date" => "13/06/2024 20:00"
+    ],
+    [
+        "title" => "prendere le medicine 3",
+        "done" => true,
+        "Date" => "13/06/2024 20:00"
+    ],
+    [
+        "title" => "prendere le medicine 4",
+        "done" => false,
+        "Date" => "13/06/2024 20:00"
+    ],
+    [
+        "title" => "prendere le medicine",
+        "done" => true,
+        "Date" => "13/06/2024 18:00"
+    ],
+    [
+        "title" => "prendere le medicine 1",
+        "done" => false,
+        "Date" => "13/06/2024 19:00"
+    ],
+    [
+        "title" => "prendere le medicine 2",
+        "done" => true,
+        "Date" => "13/06/2024 20:00"
+    ],
+    [
+        "title" => "prendere le medicine 3",
+        "done" => true,
+        "Date" => "13/06/2024 20:00"
+    ],
+    [
+        "title" => "prendere le medicine 4",
+        "done" => false,
+        "Date" => "13/06/2024 20:00"
+    ],
 ];
 header("Content-Type: application/json");
 
